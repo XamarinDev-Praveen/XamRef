567a0f5a2c43ef42de5e62c774e5069da5851bd8
Praveen kumar M <praveenpersonals01@gmail.com>
2022-03-22 22:08:13 +0530

    Testttttt

--- lib/bindings/loginBinding.dart
+++ lib/bindings/loginBinding.dart
@@ -5,6 +5,7 @@ import 'package:pssr/controllers/logincontroller.dart';
 class LoginBinding extends Bindings {
   @override
   void dependencies() {
+
     Get.lazyPut<LoginController>(
           () => LoginController(),
       //making first change
